41ccd4ad717c5ae6ed8a585438ae2c986a7438cb
oOBobbyOo <944831575.com>
2022-10-18 16:39:14 +0800

    refactor: effect and reactive

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,6 @@
 import { extend } from '@vue/shared'
 import { EffectScope, recordEffectScope } from './effectScope'
+import { TrackOpTypes, TriggerOpTypes } from './operations'
 
 type Dep = Set<any>
 type KeyToDepMap = Map<any, Dep>
@@ -9,39 +10,7 @@ export type EffectScheduler = (...args: any[]) => any
 
 export let activeEffect: ReactiveEffect | undefined
 
-export function track(target, key) {
-  if (shouldTrack && activeEffect) {
-    let depsMap = targetMap.get(key)
-    if (!depsMap) {
-      targetMap.set(target, (depsMap = new Map()))
-    }
-    let dep = depsMap.get(key)
-    if (!dep) {
-      depsMap.set(key, (dep = new Set()))
-    }
-    if (!dep.has(activeEffect!)) {
-      dep.add(activeEffect!)
-      activeEffect!.deps.push(dep)
-    }
-  }
-}
-
-export function trigger(target, key) {
-  const depsMap = targetMap.get(target)
-  if (!depsMap) {
-    // NOTE: never been tracked
-    return
-  }
-  const dep = depsMap.get(key)
-  if (!dep) return
-  for (const effect of dep) {
-    if (effect.scheduler) {
-      effect.scheduler()
-    } else {
-      effect.run()
-    }
-  }
-}
+export const ITERATE_KEY = Symbol(__DEV__ ? 'iterate' : '')
 
 export class ReactiveEffect<T = any> {
   active: boolean = true
@@ -55,11 +24,6 @@ export class ReactiveEffect<T = any> {
     public scheduler: EffectScheduler | null = null,
     scope?: EffectScope
   ) {
-    // this.fn = fn
-    // this.scheduler = scheduler
-
-    // this.onStop = onStop
-
     recordEffectScope(this, scope)
   }
 
@@ -162,17 +126,63 @@ export function stop(runner: ReactiveEffectRunner) {
 export let shouldTrack = true
 const trackStack: boolean[] = []
 
+// 暂停
 export function pauseTracking() {
   trackStack.push(shouldTrack)
   shouldTrack = false
 }
 
+// 开启
 export function enableTracking() {
   trackStack.push(shouldTrack)
   shouldTrack = true
 }
 
+// 重置
 export function resetTracking() {
   const last = trackStack.pop()
   shouldTrack = last === undefined ? true : last
+}
+
+// TODO: 依赖收集
+export function track(target: object, type: TrackOpTypes, key: unknown) {
+  if (shouldTrack && activeEffect) {
+    let depsMap = targetMap.get(key)
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+      depsMap.set(key, (dep = new Set()))
+    }
+    if (!dep.has(activeEffect!)) {
+      dep.add(activeEffect!)
+      activeEffect!.deps.push(dep)
+    }
+  }
+}
+
+// TODO: 触发依赖
+export function trigger(
+  target: object,
+  type: TriggerOpTypes,
+  key?: unknown,
+  newValue?: unknown,
+  oldValue?: unknown,
+  oldTarget?: Map<unknown, unknown> | Set<unknown>
+) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    // NOTE: never been tracked
+    return
+  }
+  const dep = depsMap.get(key)
+  if (!dep) return
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
+  }
 }
