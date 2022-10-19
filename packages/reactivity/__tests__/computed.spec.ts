b37928321cc31a753a6fc08a547403c7c62c8942
oOBobbyOo <944831575@qq.com>
2022-10-19 11:02:38 +0800

    refactor(reactivity): computed

---
+++ packages/reactivity/__tests__/computed.spec.ts
@@ -0,0 +1,67 @@
+import { computed, effect } from '../src'
+import { reactive } from '../src'
+
+describe('reactivity/computed', () => {
+  it('should return updated value', () => {
+    const value = reactive({})
+    const cValue = computed(() => value.foo)
+    expect(cValue.value).toBe(undefined)
+    value.foo = 1
+    expect(cValue.value).toBe(1)
+  })
+
+  it('should compute lazily', () => {
+    const value = reactive({})
+    const getter = jest.fn(() => value.foo)
+    const cValue = computed(getter)
+
+    // lazy
+    expect(getter).not.toHaveBeenCalled()
+
+    expect(cValue.value).toBe(undefined)
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // should not compute again
+    cValue.value
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // should not compute until needed
+    value.foo = 1
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // now it should compute
+    expect(cValue.value).toBe(1)
+    expect(getter).toHaveBeenCalledTimes(2)
+
+    // should not compute again
+    cValue.value
+    expect(getter).toHaveBeenCalledTimes(2)
+  })
+
+  it('should trigger effect', () => {
+    const value = reactive({})
+    const cValue = computed(() => value.foo)
+    let dummy
+    effect(() => {
+      dummy = cValue.value
+    })
+    expect(dummy).toBe(undefined)
+    value.foo = 1
+    expect(dummy).toBe(1)
+  })
+
+  it('should no longer update when stopped', () => {
+    const value = reactive({})
+    const cValue = computed(() => value.foo)
+    let dummy
+    effect(() => {
+      dummy = cValue.value
+    })
+    expect(dummy).toBe(undefined)
+    value.foo = 1
+    expect(dummy).toBe(1)
+    cValue.effect.stop()
+    value.foo = 2
+    expect(dummy).toBe(1)
+  })
+})
--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -3,3 +3,9 @@ export { reactive, isReactive } from './reactive'
 export { effect, stop, trigger, track } from './effect'
 
 export { ref, isRef, Ref } from './ref'
+
+export { computed, ComputedGetter, ComputedSetter } from './computed'
+
+export { effectScope, EffectScope, getCurrentScope, onScopeDispose } from './effectScope'
+
+export { TrackOpTypes, TriggerOpTypes } from './operations'
